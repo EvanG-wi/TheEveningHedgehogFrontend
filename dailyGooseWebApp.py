a8c2d4530c7fcb08e9d7e8524714e8b60a768815
divide <divide654@gmail.com>
2024-06-06 23:34:11 -0500

    streamlit folder

--- dailyGooseWebApp.py
+++ dailyGooseWebApp.py
@@ -26,8 +26,7 @@ def send_request(email_address): #send http request with email to register a new
     
     response = requests.post(url,json = request_body, headers=headers)
     if response.status_code == 200:
-        st.success(f"Request sent successfully")
-        st.write("Congratulations!\nPlease remember to check your spam folder for communications from The Evening Hedgehog")
+        st.success(f"Request sent successfully \nCongratulations!\nPlease remember to check your spam folder for communications from The Evening Hedgehog")
     else:
         st.error(f"Failed to send request. Status code: {response.status_code}")
 
