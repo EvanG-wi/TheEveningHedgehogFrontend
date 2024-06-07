b339fcbb207b9d39f8ab3a8a3af88da6fe8e4765
divide <divide654@gmail.com>
2024-06-06 23:27:33 -0500

    Update dailyGooseWebApp.py

--- dailyGooseWebApp.py
+++ dailyGooseWebApp.py
@@ -34,7 +34,7 @@ def send_request(email_address): #send http request with email to register a new
 #with open("style/style.css") as file:
     #st.markdown(f'<style>{file.read()}</style>', unsafe_allow_html=True)
 
-st.title("Subscribe to The Evening Hedghog")
+st.title("Subscribe to The \"Evening Hedghog\"")
 
 email_address = st.text_input("Enter email address")
 
