f4621bfab165878f9c5e011b2babe5d01022be21
divide <divide654@gmail.com>
2024-06-08 22:36:32 -0500

    Update dailyGooseWebApp.py

--- dailyGooseWebApp.py
+++ dailyGooseWebApp.py
@@ -6,18 +6,7 @@ from streamlit_option_menu import option_menu
 
 with open("style/style.css") as file:
     st.markdown(f'<style>{file.read()}</style>', unsafe_allow_html=True)
-
-critter_select = option_menu(
-                             menu_title=None,
-                             options=["The Daily Goose","The Evening Hedgehog"],
-                             orientation="horizontal"
-                            )
-
-
-st.title("Subscribe to "+critter_select)
-
-email_address = st.text_input("Enter email address",key='email')
-
+    
 def click_button():
     if critter_select=="The Daily Goose":
         send_request(email_address,"goose")
@@ -26,10 +15,21 @@ def click_button():
     st.session_state['email'] = ""
 
 
-submit_button = st.button("Submit email address", on_click=click_button)
 
+#Page Elements###
+critter_select = option_menu(
+                             menu_title=None,
+                             options=["The Daily Goose","The Evening Hedgehog"],
+                             orientation="horizontal"
+                            )
+st.title("Subscribe to "+critter_select)
+
+email_address = st.text_input("Enter email address",key='email')
+
+submit_button = st.button("Submit email address", on_click=click_button)
+################
 
 if critter_select=="The Daily Goose":
     st.image("ottomanEmpireCourtGoose.png")
 elif critter_select=="The Evening Hedgehog":
-    st.image("hedge_img.png")    
+    st.image("hedge_img.png")
